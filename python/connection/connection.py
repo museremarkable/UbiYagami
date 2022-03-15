fd60789ecbd7257d6fcbe5a237dcc243cb9d640e
“museremarkable” <1244667596@qq.com>
2022-03-15 23:52:58 +0800

    [connection update]

--- python/connection/connection.py
+++ python/connection/connection.py
@@ -1,7 +1,6 @@
 import asyncio
 from asyncio import StreamWriter, StreamReader
-from python.utils.wirte_logger import get_logger
-from python.connection.pollable_queue import PollableQueue
+from wirte_logger import get_logger
 from queue import Queue
 from enum import IntEnum
 import json
@@ -40,12 +39,12 @@ class ServerTCP:
         '''
         msg = await reader.readline()
         # todo: read n bytes
-        operation, args = msg.split(b'-')
-        operation = int(operation.decode())
+        # operation, args = msg.split(b'-')
+        # operation = int(operation.decode())
         addr = writer.get_extra_info('peername')
         self._add_trade(addr, reader, writer)
         # if operation == Operation.HEARTBEAT:
-        #     if addr not in self.trader2writer.keys():
+        #     if addr not in self.trader2writer.keys():R
         #         self._add_trade(addr, reader, writer)
         #     # todo:mark一下自己的名字
         #     # todo: 这里只建立连接，别的都留给exchange做
@@ -72,30 +71,54 @@ class ServerTCP:
 
     def _add_trade(self, trad_add, reader: StreamReader, writer: StreamWriter):
         self.trader2writer[trad_add] = writer
-        asyncio.create_task(self._listen_for_stream(trad_add, reader, writer))
+        asyncio.create_task(self._listen_for_stream(trad_add, reader))
+        asyncio.create_task(self._write_for_stream(trad_add, writer))
 
     async def _del_trade(self, trad_add):
-        writer = self.trader2writer[trad_add]
-        del self.trader2writer[trad_add]
+        # print('_del_trade is called')
         try:
+            writer = self.trader2writer[trad_add]
+            del self.trader2writer[trad_add]
             writer.close()
             await writer.wait_closed()
+            self.log.info(f'Delete trade{trad_add}')
         except Exception as e:
             self.log.error('Error closing client writer, ignoring.')
 
-    async def _listen_for_stream(self, trad_add, reader: StreamReader, writer: StreamWriter):
+    async def _write_for_stream(self, trad_add, writer: StreamWriter):
+        while True:
+            try:
+                data = self.response_queue.get()
+                data = data.encode()
+                if data != b'\n' and data != b'':
+                    await self._notify_all(data)
+                #     writer.write(data.encode()+b'\n')
+                #     print('Send {}'.format(data))
+                # await writer.drain()
+            except Exception as e:
+                self.log.exception(e)
+                # await self._del_trade(trad_add)
+                await asyncio.sleep(1)
+
+
+    async def _listen_for_stream(self, trad_add, reader: StreamReader):#, writer: StreamWriter):
         try:
             while True:
                 data = await asyncio.wait_for(reader.readline(), 60)
-                if data == b'':
-                    break
+                print('received {}'.format(data))
+                if data == b'\n' or data == b'':
+                    #break
+                    data = b'test'
+                    await asyncio.sleep(1)
                 self.trans_stream2exchange(trad_add, data)
-                writer.write(str(data).encode())
-                await writer.drain()
+                # writer.write(str(data).encode())
+                # await writer.drain()
             # while(data := await asyncio.wait_for(reader.readline(),60)!=b''):
             #     self.trans_stream2exchange(data)
         except Exception as e:
             self.log.error('Error reading from client.')
+            self.log.exception(e)
+            await self._del_trade(trad_add)
 
 
     def trans_stream2exchange(self,trad_add, msg):
@@ -104,36 +127,39 @@ class ServerTCP:
         :return:
         # todo: 写到queue里， put之后就返给feedback
         '''
-        operation, args = msg.split(b'-')
-        operation = int(operation.decode())
-        if operation == Operation.HEARTBEAT:
-            pass
-        elif operation == Operation.RAW_ORDER:
-            order = json.loads(args)
-            self.order_queue.put(order)
-            self._notify_all(str(Operation.RAW_ORDER_RESPONSE)+'-'+order['count'])
-            # todo: wait_response
-        elif operation == Operation.TRADE_ORDER_RESPONSE:
-            # todo: 返回对应的序号
-            pass
-        # todo: 如果考虑断连，后面可以通过加response的方式进行测试
+        #operation, args = msg.split(b'-')
+        #operation = int(operation.decode())
+        #print('receive{}'.format(msg))
+        self.order_queue.put(msg)
+        self.response_queue.put(msg)
+        # if operation == Operation.HEARTBEAT:
         #     pass
-        #     self.response_queue.put(b'SUCC'+'-')
-        else:
-            self.log.error('Got invalid command from client, disconnecting.')
-            self._del_trade(trad_add)
+        # elif operation == Operation.RAW_ORDER:
+        #     order = json.loads(args)
+        #     self.order_queue.put(order)
+        #     self._notify_all(str(Operation.RAW_ORDER_RESPONSE)+'-'+order['count'])
+        #     # todo: wait_response
+        # elif operation == Operation.TRADE_ORDER_RESPONSE:
+        #     # todo: 返回对应的序号
+        #     pass
+        # # todo: 如果考虑断连，后面可以通过加response的方式进行测试
+        # #     pass
+        # #     self.response_queue.put(b'SUCC'+'-')
+        # else:
+        #     self.log.error('Got invalid command from client, disconnecting.')
+        #     self._del_trade(trad_add)
         # self.order_queue.put(json.loads(msg))
         # self._send_response(b'RESPONSE'+msg)
         #收到信息
 
-    def send_trade(self):
-        while True:
-            data = self.response_queue.get()
-            if data:
-                msg = str(Operation.TRADE_ORDER).encode()+data.encode()
-                self._notify_all(msg)
-            else:
-                break
+    # def send_trade(self):
+    #     while True:
+    #         data = self.response_queue.get()
+    #         if data:
+    #             msg = str(Operation.TRADE_ORDER).encode()+data.encode()
+    #             self._notify_all(msg)
+    #         else:
+    #             break
 
     async def _notify_all(self, msg):
         # todo: response queue
@@ -141,8 +167,12 @@ class ServerTCP:
         inactive_trade = []
         for addr, writer in self.trader2writer.items():
             try:
-                writer.write(msg.decode())
+                if type(msg) == bytes:
+                    writer.write(msg)
+                else:
+                    writer.write(msg.encode())
                 await writer.drain()
+                await asyncio.sleep(1)
             except ConnectionError as e:
                 self.log.exception('Could not write to client.', exc_info=e)
                 inactive_trade.append(addr)
@@ -150,9 +180,15 @@ class ServerTCP:
 
 
 async def main():
-    order_queue = PollableQueue()
-    response_queue = PollableQueue()
-    ServerTCP(order_queue, response_queue)
+    order_queue = Queue()
+    response_queue = Queue()
+    count = 0
+    while count <= 50:
+        count += 1
+        order_queue.put(str(count)+'\n')
+        response_queue.put(str(count)+'\n')
+    server = ServerTCP(order_queue, response_queue)
+    await server.server_connection('127.0.0.1', 8000)
 
 
 asyncio.run(main())
--- python/connection/tcp_client.py
+++ python/connection/tcp_client.py
@@ -1,7 +1,6 @@
 import asyncio
 from asyncio import StreamWriter, StreamReader
-from python.utils.wirte_logger import get_logger
-from python.connection.pollable_queue import PollableQueue
+from wirte_logger import get_logger
 from queue import Queue
 import json
 import socket
@@ -9,9 +8,10 @@ import socket
 class ClientTCP:
     def __init__(self, order_queue, response_queue):
         #todo: queue让外面传进来
-        self.ip_address = ["127.0.0.1", "127.0.0.1"]
+        self.ip_address = ["127.0.0.1"]
+        self.alive = {}
         self.exchange2writer = {}
-        self.binding_port = 8888
+        self.binding_port = 8000
         "0 is normal, differnet number corresponding different stastus"
         self.ports_status = [0, 0, 0]
         self.log = get_logger(__name__, filename='streaming_client')
@@ -19,48 +19,84 @@ class ClientTCP:
         self.order_queue = order_queue
         self.response_queue = response_queue
 
-    async def send_message(self, writer: StreamWriter):
+    async def send_message(self, host, writer: StreamWriter):
         '''send info to server'''
         message = self.response_queue.get()
+        print('Send {}'.format(message))
         while message:
-            writer.write((message + '\n').encode())
+            writer.write((str(message).encode()) + b'\n')
             await writer.drain()
-        writer.close()
+            message = self.response_queue.get()
+            await asyncio.sleep(2)
+        # del self.alive[host]
+        # writer.close()
 
-    async def listen_for_messages(self, reader: StreamReader):
-        while (data := await reader.readline()) != b'':
-            await self.response_queue.put(data.decode())
-        await self.response_queue.put('Server closed connection.')
+    async def listen_for_messages(self, host, reader: StreamReader):
+        while True:
+            data = await asyncio.wait_for(reader.readline(), 60)
+            if data != b'\n' and data != b'':
+                print('Recieved {}'.format(data))
+                self.response_queue.put(data+b'res')
+                await asyncio.sleep(1)
+            else:
+                # del self.alive[host]
+                #break
+                await asyncio.sleep(2)
+        print('Server closed connection.')
 
     async def client_connection(self, host, port):
-        "send order to two exchanger? "
+        print('prepare connection')
         reader, writer = await asyncio.open_connection(host, port)
-        asyncio.create_task(self.listen_for_messages(reader))
-        asyncio.create_task(self.send_message(writer))
-        return reader, writer
+        self.alive[host] = True
+        writer.write(f'CONNECT {port}\n'.encode())
+        await writer.drain()
+        asyncio.create_task(self.send_message(host, writer))
+        asyncio.create_task(self.listen_for_messages(host, reader))
+
+        # return reader, writer
 
     async def reconnection(self, host, port):
         while True:
-            self.log.info('Connecting to server {}:{}'.format(host, port))
-            try:
-                await self.client_connection(host, port)
-            except ConnectionRefusedError:
-                self.log.info('Connection to server {}:{} failed!'.format(host, port))
-            except asyncio.TimeoutError:
-                self.log.error('Connection to server {}:{} timed out!'.format(host, port))
-            else:
-                self.log.error('Connection to server {}:{} is closed.'.format(host, port))
+
+            if host not in self.alive.keys():
+                self.log.info('Connecting to server {}:{}'.format(host, port))
+                try:
+                    await self.client_connection(host, port)
+                except ConnectionRefusedError:
+                    self.log.error('Connection to server {}:{} failed!'.format(host, port))
+                except asyncio.TimeoutError:
+                    self.log.error('Connection to server {}:{} timed out!'.format(host, port))
+                else:
+                    self.log.error('Connection to server {}:{} is closed.'.format(host, port))
             await asyncio.sleep(2)
 
     async def run_main(self):
         #for ip in self.ip_address:
+        #await self.client_connection('127.0.0.1', 8888)
         coros = [self.reconnection(host, self.binding_port) for host in self.ip_address]
         await asyncio.gather(*coros)
 
+    async def run_without_retry(self):
+        #await self.client_connection('127.0.0.1', 8000)
+        reader, writer = await asyncio.open_connection('127.0.0.1', 8000)
+        # # self.alive[host] = True
+        writer.write(f'CONNECT {8000}\n'.encode())
+        await writer.drain()
+
+        asyncio.create_task(self.send_message('127.0.0.1', writer))
+        asyncio.create_task(self.listen_for_messages('127.0.0.1', reader))
+
 
 if __name__ == '__main__':
-    order_queue = PollableQueue()
-    response_queue = PollableQueue()
+    order_queue = Queue()
+    response_queue = Queue()
+    count = 100
+    while count <= 120:
+        count += 1
+        order_queue.put(str(count)+'\n')
+        response_queue.put(str(count)+'\n')
+    # Client_run = ClientTCP(order_queue, response_queue)
+    # asyncio.get_event_loop().run_until_complete(Client_run.run_without_retry())
     loop = asyncio.get_event_loop()
     Client_run = ClientTCP(order_queue, response_queue)
     loop.run_until_complete(Client_run.run_main())

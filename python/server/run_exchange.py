f74727d454837a5eb70ff28183df607b29dd1ea5
Leon8 <zhyile@outlook.com>
2022-03-17 12:01:48 +0800

    merge

--- python/server/run_exchange.py
+++ python/server/run_exchange.py
@@ -1,28 +1,20 @@
-<<<<<<< HEAD
+
 import sys
 sys.path.append("..")
 sys.path.append("../utils")
-# from connection.connection import server
-from simple_server_test import server
+from connection.connection import server
+# from simple_server_test import server
 from server import MatchingEngine
 from multiprocessing import Queue, Process
 from connection.connect_wrapper import connect
-# from connection.connection import ServerTCP
-from connection.connect_wrapper import connect
 
-# path = os.path.join(os.path.dirname(__file__), os.pardir)
-# sys.path.append(path)
 
 if __name__ == "__main__":
 
-	trade_file = "trade_result"
 	close_file = "../../data/100x10x10/price1.h5"
 	data_path = "../../data/100x10x10"
-	host = '106.15.11.226'
-	port = 12345
 
 	recv_queue, send_queue = Queue(), Queue()
-	# server = ServerTCP(recv_queue, send_queue, host, port)
 	connect_kernel = connect(recv_queue=recv_queue, send_queue=send_queue)
 	engine = MatchingEngine(connect=connect_kernel, path_close=close_file)
 
@@ -31,7 +23,7 @@ if __name__ == "__main__":
 	process_list.append(p)
 	p.start()
 	print('Add Process server TCP')
-	p = Process(target=server, args=(recv_queue, send_queue, ))#host, port
+	p = Process(target=server, args=(recv_queue, send_queue, ))
 	process_list.append(p)
 	p.start()
 

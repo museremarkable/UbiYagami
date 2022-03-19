d1276f8f102525f308032e0dadac6edbb6346636
Leon8 <zhyile@outlook.com>
2022-03-19 16:25:45 +0800

    server refine

--- python/server/run_multi_exchange.py
+++ python/server/run_multi_exchange.py
@@ -11,7 +11,7 @@ from multiprocessing import Queue, Process
 from connection.connect_wrapper import connect
 
 
-def exchange(recv_queue, send_queue):
+def exchange(recv_queue, send_queue, close_file):
 	connect_kernel = connect(recv_queue=recv_queue, send_queue=send_queue)
 	engine = MatchingEngine(connect=connect_kernel, path_close=close_file)
 	engine.engine_main_thread(matching_threads=2)
@@ -20,12 +20,11 @@ def exchange(recv_queue, send_queue):
 if __name__ == "__main__":
 	port = [62345, 62346, 62347]
 	close_file = "../../data/100x10x10/price1.h5"
-	data_path = "../../data/100x10x10"
 
 	recv_queue, send_queue = Queue(), Queue()
 
 	process_list = []
-	p = Process(target=exchange, args=(recv_queue, send_queue ))
+	p = Process(target=exchange, args=(recv_queue, send_queue, close_file))
 	process_list.append(p)
 	p.start()
 	print('Add Process server TCP')
